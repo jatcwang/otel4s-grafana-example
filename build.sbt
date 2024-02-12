0012ab9ca7bf60265a2faedee4b5caf0abab8932
Jacob Wang <jatcwang@gmail.com>
2024-02-12 20:48:58 +0000

    switch to http4s-otel4s-middleware

--- build.sbt
+++ build.sbt
@@ -29,14 +29,14 @@ lazy val root = (project in file("."))
 
 lazy val commonDeps = Seq(deps.cats, deps.catsEffect)
 lazy val circeDeps = Seq(deps.circe, deps.circeGeneric)
-lazy val http4sDeps = Seq(deps.http4sServer, deps.http4sDsl, deps.http4sCirce)
+lazy val http4sDeps = Seq(deps.http4sServer, deps.http4sDsl, deps.http4sCirce, deps.http4sOtel4sMiddleware)
 lazy val otelDeps = Seq(deps.otel4s, deps.otelExporter, deps.otelSdk)
 
 lazy val deps = new {
   val catsVersion = "2.10.0"
   val catsEffectVersion = "3.5.3"
   val circeVersion = "0.14.6"
-  val http4sVersion = "1.0.0-M40"
+  val http4sVersion = "0.23.24"
   val otel4sVersion = "0.4.0"
   val otelVersion = "1.34.1"
   val slf4jVersion = "2.0.5"
@@ -48,6 +48,7 @@ lazy val deps = new {
   val circe = "io.circe" %% "circe-core" % circeVersion
   val circeGeneric = "io.circe" %% "circe-generic" % circeVersion
 
+  val http4sOtel4sMiddleware = "org.http4s" %% "http4s-otel4s-middleware" % "0.3.0"
   val http4sServer = "org.http4s" %% "http4s-ember-server" % http4sVersion
   val http4sDsl = "org.http4s" %% "http4s-dsl" % http4sVersion
   val http4sCirce = "org.http4s" %% "http4s-circe" % http4sVersion
--- src/main/scala/Server.scala
+++ src/main/scala/Server.scala
@@ -1,23 +1,23 @@
-import cats.effect.std.Random
 import cats.effect.*
+import cats.effect.std.Random
 import com.comcast.ip4s.{ipv4, port}
 import io.opentelemetry.api.GlobalOpenTelemetry
 import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.otel4s.middleware.ServerMiddleware
 import org.http4s.server.Server
-import org.typelevel.log4cats.LoggerFactory
 import org.typelevel.otel4s.java.OtelJava
 import org.typelevel.otel4s.metrics.Meter
 import org.typelevel.otel4s.trace.Tracer
 
-import org.typelevel.log4cats.slf4j.Slf4jFactory
-
 object Server extends IOApp {
   private def app[F[_]: Async: LiftIO]: Resource[F, Server] =
-    given LoggerFactory[F] = Slf4jFactory.create[F]
     for {
       given Random[F] <- Resource.eval(Random.scalaUtilRandom[F])
       otel <- Resource
         .eval(Async[F].delay(GlobalOpenTelemetry.get))
+        .evalTap(otel =>
+          Async[F].delay(println(otel.getPropagators.getTextMapPropagator.fields()))
+        )
         .evalMap(OtelJava.forAsync[F])
       serviceName = "otel4s-grafana-example"
       given Meter[F] <- Resource.eval(otel.meterProvider.get(serviceName))
@@ -28,7 +28,7 @@ object Server extends IOApp {
         .default[F]
         .withHost(ipv4"0.0.0.0")
         .withPort(port"8080")
-        .withHttpApp(route.orNotFound.traced)
+        .withHttpApp(ServerMiddleware.default[F].buildHttpApp(route.orNotFound))
         .build
     } yield server
 
